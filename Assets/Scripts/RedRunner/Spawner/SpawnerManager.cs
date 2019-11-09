a7f6d0b61bc845eb09e69eb71e87297caca5ba19
Wilson Wu <wilsoncwu@gmail.com>
2019-11-09 18:03:17 -0500

    camera scroller imported, as well as basic setup

---
+++ Assets/Scripts/RedRunner/Spawner/SpawnerManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerManager : MonoBehaviour
+{
+    [SerializeField]
+    BitBenderGames.TouchInputController inputController;
+    [SerializeField]
+    BitBenderGames.MobileTouchCamera mobileTouchCamera;
+
+    void EnableScrolling()
+    {
+        inputController.gameObject.SetActive(true);
+        mobileTouchCamera.gameObject.SetActive(true);
+    }
+
+    void DisableScrolling()
+    {
+        mobileTouchCamera.gameObject.SetActive(false);
+        inputController.gameObject.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
