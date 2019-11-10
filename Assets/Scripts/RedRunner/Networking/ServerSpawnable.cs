2c056999489daaaef47146e631c78157287e0711
Shane Hickman <srhickma@edu.uwaterloo.ca>
2019-11-10 18:49:58 -0500

    RPC from terrain generator

--- Assets/Scripts/RedRunner/Networking/ServerSpawnable.cs
+++ Assets/Scripts/RedRunner/Networking/ServerSpawnable.cs
@@ -6,13 +6,26 @@ namespace RedRunner.Networking
 	/// Helper component for game object spawned by the server which should synchronize to clients.
 	/// </summary>
 	[RequireComponent(typeof(Mirror.NetworkIdentity))]
-	public class ServerSpawnable : MonoBehaviour
+	public class ServerSpawnable : Mirror.NetworkBehaviour
 	{
+		public bool ping = false;
+
 		public virtual void Awake()
 		{
-			if (NetworkManager.IsServer)
+			if (NetworkManager.IsServer && !ping)
 			{
 				NetworkManager.Spawn(gameObject);
+			}
+		}
+
+		[Mirror.ClientRpc]
+		void RpcPing2() {
+			Debug.Log("Ping2!");
+		}
+
+		public void Update() {
+			if (NetworkManager.IsServer && ping) {
+				RpcPing2();
 			}
 		}
 	}
