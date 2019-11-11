8a87bf2e613d2c1e0b347df7e14337d01fb8e3f0
salberico <sam.alberico@gmail.com>
2019-11-11 00:18:14 -0500

    added sprite renderer copying tree thing

--- Assets/Scripts/RedRunner/UI/UIScreen/SpawnerScreen.cs
+++ Assets/Scripts/RedRunner/UI/UIScreen/SpawnerScreen.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using RedRunner.Utilities;
 
 namespace RedRunner.UI
 {
@@ -21,10 +22,32 @@ namespace RedRunner.UI
             base.UpdateScreenStatus(open);
         }
 
+        private void DFSClone(Transform source, Transform dest)
+        {
+            dest.localScale = source.localScale;
+            dest.localPosition = source.localPosition;
+            dest.localRotation = source.localRotation;
+            SpriteRenderer spriteRenderer = source.GetComponent<SpriteRenderer>();
+            if (null != spriteRenderer)
+            {
+                dest.gameObject.AddComponent<SpriteRenderer>(spriteRenderer); // copy params with reflection
+            }
+            
+            for (int i = 0; i < source.childCount; ++i)
+            {
+                Transform child = source.GetChild(i);
+                GameObject newBlock = new GameObject(child.name, typeof(RectTransform));
+                newBlock.transform.SetParent(dest);
+                DFSClone(child, newBlock.transform);
+            }
+        }
+
         public void AddBlock(Block block)
         {
             GameObject newBlock = new GameObject(block.name, typeof(RectTransform));
             newBlock.transform.SetParent(grid.transform, false);
+            DFSClone(block.transform, newBlock.transform);
+            newBlock.transform.localScale = newBlock.transform.localScale * 15f;
         }
     }
 }
