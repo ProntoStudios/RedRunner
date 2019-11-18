3a0abe46467ed92964bd4198cf02cf497d79c84f
Manuel Dionne <mandio0302@gmail.com>
2019-11-18 02:40:30 -0400

    Added bomb

---
+++ Assets/Scripts/RedRunner/Spawner/BombExplosion.cs
@@ -0,0 +1,37 @@
+﻿using RedRunner.TerrainGeneration;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombExplosion : MonoBehaviour
+{
+    [SerializeField]
+    protected float m_ExplodeRadius = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // boom goes the dynamite
+    public void Explode()
+    {
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position, m_ExplodeRadius);
+        for (int i = 0; i < hitColliders.Length; i++)
+        {
+
+            if (hitColliders[i].gameObject.GetComponent(typeof(Deletable)) as Deletable != null)
+            {
+                Destroy(hitColliders[i].gameObject);
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
