c35637ccfe78a6d1b581e7b82dc25aeb47a5a272
Wilson Wu <wilsoncwu@gmail.com>
2019-11-17 19:10:36 -0500

    basic progress bar

---
+++ Assets/Scripts/RedRunner/UI/UIScoreBar.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScoreBar : MonoBehaviour
+{
+    [SerializeField]
+    Slider slider;
+
+    // set a number in range [0,1]
+    public void SetPercentage(float val)
+    {
+        slider.value = val;
+    }
+}
