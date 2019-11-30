1ae92b199c024eea0d0f8eabdd8aac47b69e173e
Colin Daly <dalyco884@gmail.com>
2019-11-30 18:39:52 -0500

    switch wall slide logic to fixedupdate

--- Assets/Scripts/RedRunner/Characters/RedCharacter.cs
+++ Assets/Scripts/RedRunner/Characters/RedCharacter.cs
@@ -445,7 +445,17 @@ namespace RedRunner.Characters
             m_Speed = new Vector2(Mathf.Abs(m_Rigidbody2D.velocity.x), Mathf.Abs(m_Rigidbody2D.velocity.y));
         }
 
-        void Update ()
+		private void FixedUpdate()
+		{
+			if (m_Rigidbody2D.velocity.y < 0f && m_IsWallSliding)
+			{
+				Vector2 velocity = m_Rigidbody2D.velocity;
+				velocity.y *= m_WallSlideSlowdown;
+				m_Rigidbody2D.velocity = velocity;
+				Debug.Log("U: " + m_Rigidbody2D.velocity);
+			}
+		}
+		void Update ()
 		{
 			ComputeTarget();
 
@@ -457,13 +467,6 @@ namespace RedRunner.Characters
 			if ( transform.position.y < 0f )
 			{
 				Die ();
-			}
-
-			if ( m_Rigidbody2D.velocity.y < 0f && m_IsWallSliding)
-			{
-				Vector2 velocity = m_Rigidbody2D.velocity;
-				velocity.y *= m_WallSlideSlowdown;
-				m_Rigidbody2D.velocity = velocity;
 			}
 
             UpdateMovement();
